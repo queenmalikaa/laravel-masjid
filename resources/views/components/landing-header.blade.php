a7a291d3d3e8f16de2792f77ec3b2b3d8a77937b
queenmalikaa <gerhanagerhano@gmail.com>
2024-09-24 07:41:40 +0700

    Add files via upload

---
+++ resources/views/components/landing-header.blade.php
@@ -0,0 +1,15 @@
+<div class="bg-yellow-500 w-full p-4">
+    <nav class="w-full">
+        <ul class="flex gap-4 justify-center">
+            <li class="border border-white rounded p-2 hover:bg-yellow-600 transition duration-200">
+                <a href="{{route('landing')}}" class="text-white text-md">Home</a>
+            </li>
+            <li class="border border-white rounded p-2 hover:bg-yellow-600 transition duration-200">
+                <a href="{{route('pemasukan.create')}}" class="text-white text-md">Pemasukan</a>
+            </li>
+            <li class="border border-white rounded p-2 hover:bg-yellow-600 transition duration-200">
+                <a href="{{route('login')}}" class="text-white text-md">Login</a>
+            </li>
+        </ul>
+    </nav>
+</div>
